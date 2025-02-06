e90c6664b8c3f7f5df2637178ab56a6b69e366bb
Jubilant K J <jubilantkj2018@gmail.com>
2025-02-06 15:38:43 +0530

    created cart.service.ts and updated it

--- 02-front-end/angular-ecommerce/src/app/services/cart.service.ts
+++ 02-front-end/angular-ecommerce/src/app/services/cart.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
   constructor() { }
+
+  addToCart(theCartItem: CartItem){
+    //check if we have already have thhe item in our cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = new CartItem("", "", "", 0);
+
+    if(this.cartItems.length>0){
+      //find the item in the cart based on item id
+
+    //check if we found it
+    }
+    
+  }
 }
